42753929af13f299c96ad11211b81b1add3066e1
Yacin Kilani <yassine.kilani123@gmail.com>
2025-06-19 15:35:27 +0100

    W4 React multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+
 const list = [
   {
     title: "React",
     url: "https://reactjs.org/",
     author: "Jordan Walke",
-    num_comment: 3,
+    num_comments: 3,
     points: 4,
     objectID: 0,
   },
@@ -12,143 +13,58 @@ const list = [
     title: "Redux",
     url: "https://redux.js.org/",
     author: "Dan Abramov, Andrew Clark",
-    num_comment: 2,
+    num_comments: 2,
     points: 5,
     objectID: 1,
   },
 ];
 
-function App() {
+function List({ items }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments} comments</span>
+          <span>{item.points} points</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Search({ searchTerm, onSearch }) {
   return (
     <div>
-      <h1>Hello World</h1>
+      <label htmlFor="search">Search: </label>
+      <input
+        id="search"
+        type="text"
+        value={searchTerm}
+        onChange={(event) => onSearch(event.target.value)}
+      />
     </div>
   );
 }
 
-
-
 function App() {
-  const title = "React";
-  return (
-    <div>
-      <h1>Hello {title}</h1>
-    </div>
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const filteredList = list.filter((item) =>
+    item.author.toLowerCase().includes(searchTerm.toLowerCase())
   );
-}
 
-
-
-const title = "React";
-function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
-    </div>
-  );
-}
-
-
-const title = "React";
-
-function App() {
-  return (
-    <div>
-      <h1>Hello {title}</h1>
-    </div>
-  );
-}
-
-
-const title = "React";
-
-function App() {
-  return (
-    <div>
-      <h1>Hello {title}</h1>
-      <label htmlFor="search">Search</label>
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return <li key={item.objectID}>{item.title}</li>;
-        })}
-      </ul>
+      <List items={filteredList} />
     </div>
   );
 }
 
-
-
-const volume = {
-  greeting: "Hello",
-  title: "React",
-};
-
-function App() {
-  return (
-    <div>
-      <h1>{volume.greeting} {volume.title}</h1>
-      <label htmlFor="search">Search</label>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
-      <hr />
-      <ul>
-        {list.map(function (item) {
-          return <li key={item.objectID}>{item.title}</li>;
-        })}
-      </ul>
-    </div>
-  );
-}
-
-
-
-function getTitle(title) {
-  return title;
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello {getTitle("React")}</h1>
-      <label htmlFor="search">Search</label>
-      <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
-      <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comment}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-}
-
-
-function getTitle(title) {
-  return title;
-}
-function App() {
-  return (
-    <div>
-      <h1>Hello {getTitle("react")}</h1>
-      <label htmlFor="search">Search:</label>Add commentMore actions
-      <input id="search" type="text"></input>
-    </div>
-  );
-}
 export default App;
